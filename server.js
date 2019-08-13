f8a1790d2dec079e29b1cf3f853e0e8ebd8eb2d7
afranco <andrea.franco@overit.it>
2019-08-13 12:12:45 +0200

    Added database connection info

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host : 'postgresql-tapered-74712',
-        user : 'andreafranco',
-        password : '',
-        database : 'smart-brain-db'
+        host : process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
